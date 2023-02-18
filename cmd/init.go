c6f6919d304b2c550d3c881b778b0a34cfd2eaba
tiklupxgit <tiklup1729@gmail.com>
2023-02-19 00:16:49 +0530

    feat: init, validate and run cmds added | sample collab files added

---
+++ cmd/init.go
@@ -0,0 +1,40 @@
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+// this cmd will generate colaboration sample config files in folder named "helloworld_configs" in same directory
+// configs files name will be : "collaborator1_tables.yaml", "collaborator2_tables.yaml", "contract.yaml"
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("init called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
