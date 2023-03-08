5679b8767cd423bbf7c337275411e55949e1f38c
Shubh Karman Singh <54154054+qascade@users.noreply.github.com>
2023-03-08 16:40:49 +0530

    feat: all yamls are parsing

---
+++ collaboration/collaboration.go
@@ -0,0 +1,125 @@
+package collaboration
+
+import (
+	"bytes"
+	"fmt"
+	"io/fs"
+	"os"
+	"regexp"
+
+	"gopkg.in/yaml.v3"
+
+	"github.com/qascade/dcr/collaboration/contract"
+)
+
+// Collaboration Package structure
+// should contain three files
+// 1. contract.yaml
+// 2. two *_tables.yaml for each collaborator mentioned in contract.yaml
+// This function for now, is to validate names across over the contract package
+
+type CollaborationPackage struct {
+	collabPkg fs.FS
+}
+
+// A Type of Collaboration Package must implement CollaborationParser interface
+type CollaborationParser interface {
+	Parse(path string) (*contract.ContractSpec, *[]contract.TablesContractSpec, error)
+}
+
+
+func (c *CollaborationPackage) Parse(path string) (*contract.ContractSpec, *[]contract.TablesContractSpec, error) {
+	c.collabPkg = os.DirFS(path)
+	var(
+		cSpec contract.Spec
+		tSpec contract.Spec
+		cResult contract.ContractSpec
+		tResults []contract.TablesContractSpec
+	)
+	var tablesRE = regexp.MustCompile(`.*_tables\.yaml`)
+	var contractRE = regexp.MustCompile(`contract\.yaml`)
+	err := fs.WalkDir(c.collabPkg, ".", func(relpath string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		if contractRE.MatchString(d.Name()) {
+			contractB, err := os.ReadFile(path + "/" + d.Name())
+			if err != nil {
+				return fmt.Errorf("error while reading the file: %w", err)
+			}
+			cSpec, err = ParseSpec(contractB, contract.ContractSpecType)
+			if err != nil {
+				return fmt.Errorf("error while reading the file: %w", err)
+			}
+			cBytes, err := yaml.Marshal(cSpec)
+			if err != nil {
+				return fmt.Errorf("error while marshalling the file: %w", err)
+			}
+			err = yaml.Unmarshal(cBytes, &cResult)
+			if err != nil {
+				return fmt.Errorf("unable to unmarshal to TablesContractSpec, %s", d.Name())
+			}
+		} else if tablesRE.MatchString(d.Name()) {
+			testContractB, err := os.ReadFile(path + "/" + d.Name())
+			if err != nil {
+				return fmt.Errorf("error while reading the file: %w", err)
+			}
+			tSpec, err = ParseSpec(testContractB, contract.TablesContractSpecType)
+			if err != nil {
+				return fmt.Errorf("error while reading the file: %w", err)
+			}
+			tBytes, err := yaml.Marshal(tSpec)
+			if err != nil {
+				return fmt.Errorf("error while marshalling the file: %w", err)
+			}
+			var tResult contract.TablesContractSpec
+			err = yaml.Unmarshal(tBytes, &tResult)
+			if err != nil {
+				return fmt.Errorf("unable to unmarshal to TablesContractSpec, %s", d.Name())
+			}
+			tResults = append(tResults, tResult)
+		} else {
+			return fmt.Errorf("invalid file, file type with type name not supported, %s", d.Name())
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, nil, fmt.Errorf("error while walking the directory: %w", err)
+	}
+	return &cResult, &tResults, nil
+}
+
+func ParseSpec(fileYaml []byte, specType contract.SpecType) (contract.Spec, error) {
+	var bs contract.Spec
+	if specType == contract.ContractSpecType {
+		bs = contract.ContractSpec{}
+	} else {
+		bs = contract.TablesContractSpec{}
+	}
+	err := UnmarshalStrict(fileYaml, &bs)
+	if err != nil {
+		var bs2 contract.Spec
+		err2 := yaml.Unmarshal(fileYaml, &bs2)
+		if err2 != nil {
+			return bs, fmt.Errorf("error parsing yaml: %w", err2)
+		}
+		partialBuildSpecYaml, err3 := yaml.Marshal(bs2)
+		if err3 != nil {
+			return bs, fmt.Errorf("error marshaling partial build spec: %w", err3)
+		}
+		return bs, fmt.Errorf(
+			"error parsing yaml.  Parse result:\n%s\nParse error:%s",
+			partialBuildSpecYaml,
+			err)
+	}
+	return bs, err
+}
+
+func UnmarshalStrict(in []byte, out interface{}) (err error) {
+	knownFieldsDecoder := yaml.NewDecoder(bytes.NewReader(in))
+	knownFieldsDecoder.KnownFields(true)
+	return knownFieldsDecoder.Decode(out)
+}
--- tests/contract_test.go
+++ tests/contract_test.go
@@ -1,27 +1,41 @@
 package tests
 
 import (
-	"github.com/stretchr/testify/require"
+	"fmt"
 	"testing"
-
-	"github.com/qascade/dcr/core/contract"
+	"github.com/stretchr/testify/require"
+	 
+	collab "github.com/qascade/dcr/collaboration"
 )
 
 type testPackage struct {
 	path string
 }
 
+
 func TestContract(t *testing.T) {
-	var testStructs []testPackage = []testPackage{
+	var testStructs = []testPackage{
 		{
 			path: "../samples/small_collab",
-		},
-	}
-	for _, testStruct := range testStructs {
-		tempContract, err := contract.ParseContract(testStruct.path)
-		require.NoError(t, err)
-		err = tempContract.Validate()
-		require.NoError(t, err)
+		},	
 	}
 
+	for _, testStruct := range testStructs {
+		testParsing(t, testStruct)
+	}
+
+}
+
+func testParsing(t *testing.T, testStruct testPackage) {
+	var collabPkg collab.CollaborationParser = &collab.CollaborationPackage{}
+	cSpec, tSpecs, err := collabPkg.Parse(testStruct.path)
+	require.NoError(t, err)
+	fmt.Println(cSpec)
+	fmt.Println()
+	for _, tSpec := range *tSpecs {
+		fmt.Println(tSpec)
+		fmt.Println()
+	}
+	require.NotNil(t, cSpec)
+	require.NotNil(t, tSpecs)
 }
