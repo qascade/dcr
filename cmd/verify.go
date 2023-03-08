2fbc07fe885032eb1059314e133caa6408e1f0fc
shisuimadara <2020ucs0090@iitjammu.ac.in>
2023-03-08 23:41:40 +0530

    added verify to cli and completed verification

---
+++ cmd/verify.go
@@ -0,0 +1,25 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var verifyCmd = &cobra.Command{
+	Use:   "Verification of contracts",
+	Short: "Verifies if the parties in business have mutually agreed upon the same rules",
+	Long:  `A cli tool to compare the contracts. The required parameter is the path where the contract.yaml is present`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("run called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(verifyCmd)
+	verifyCmd.Flags().StringVarP(&contractPath, "path", "f", "", "Path to the contract file")
+
+}
