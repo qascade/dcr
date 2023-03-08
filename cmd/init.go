9f439bbdf812e662fe72afc3493af66fdd42bf32
tiklupxgit <tiklup1729@gmail.com>
2023-03-08 14:20:06 +0530

    feat: added function dublicateSampleFolder

--- cmd/init.go
+++ cmd/init.go
@@ -3,6 +3,10 @@ package cmd
 
 import (
 	"fmt"
+	"embed"
+    "io/fs"
+    "os"
+    "path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -22,19 +26,87 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Println("init called")
+		dublicateSampleFolder()
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(initCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+//go:embed source_folder/*
+var sampleFolder embed.FS
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+func dublicateSampleFolder(){
+	destDir := "temp2"
+	sourceFolderPath := "temp"
+	//samples\hw_collaboration
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	err = copyFolder(sourceFolderPath, destDir)    
+}
+
+func copyFolder(sourceFolderPath, destinationFolderPath string) error {
+	// Get the current working directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("could not get current working directory: %w", err)
+	}
+
+	// Construct the absolute paths to the source and destination folders
+	sourceAbsPath := filepath.Join(cwd, sourceFolderPath)
+	destAbsPath := filepath.Join(cwd, destinationFolderPath)
+
+	// Open the source folder
+	source, err := embed.Embed(sourceAbsPath)
+	if err != nil {
+		return fmt.Errorf("could not open source folder: %w", err)
+	}
+
+	// Create the destination folder if it doesn't exist
+	if _, err := os.Stat(destAbsPath); os.IsNotExist(err) {
+		if err := os.MkdirAll(destAbsPath, 0755); err != nil {
+			return fmt.Errorf("could not create destination folder: %w", err)
+		}
+	}
+
+	// Copy files from the source to the destination folder
+	return filepath.Walk(sourceAbsPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return fmt.Errorf("could not access file %q: %w", path, err)
+		}
+
+		// Skip directories and hidden files
+		if info.IsDir() || filepath.Base(path)[0] == '.' {
+			return nil
+		}
+
+		// Open the source file
+		file, err := source.Open(path)
+		if err != nil {
+			return fmt.Errorf("could not open source file %q: %w", path, err)
+		}
+		defer file.Close()
+
+		// Create the destination file
+		destPath := filepath.Join(destAbsPath, path[len(sourceAbsPath):])
+		destDir := filepath.Dir(destPath)
+		if _, err := os.Stat(destDir); os.IsNotExist(err) {
+			if err := os.MkdirAll(destDir, 0755); err != nil {
+				return fmt.Errorf("could not create destination directory %q: %w", destDir, err)
+			}
+		}
+		dest, err := os.Create(destPath)
+		if err != nil {
+			return fmt.Errorf("could not create destination file %q: %w", destPath, err)
+		}
+		defer dest.Close()
+
+		// Copy the file contents
+		if _, err := io.Copy(dest, file); err != nil {
+			return fmt.Errorf("could not copy file contents from %q to %q: %w", path, destPath, err)
+		}
+
+		return nil
+	})
 }
