6e8ed4f5151d679c746d5bcd77e1dae755d789e2
tiklupxgit <tiklup1729@gmail.com>
2023-04-02 18:36:26 +0530

    feat: dest_path flag added in dcr init cmd

--- cmd/init.go
+++ cmd/init.go
@@ -26,13 +26,17 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Println("init called")
-		generateCollabFiles()
+		destPath := cmd.Flag("dest").Value.String()
+		generateCollabFiles(destPath)
 		return nil
 	},
 }
 
+var collabDestPath string
+
 func init() {
 	rootCmd.AddCommand(initCmd)
+	initCmd.Flags().StringVarP(&collabDestPath, "dest", "d", "", "Destination path to initialize collab pkg")
 }
 
 //go:embed collabsamples/hw_collaboration/*
@@ -41,48 +45,56 @@ var f embed.FS
 // this func, uses embed package and provides files inside [samples/hw_collaboration/*]
 // during run time.
 // Files are then saved in a new directory
-func generateCollabFiles() {
+func generateCollabFiles(destPath string) error {
 	contractFile, err := f.ReadFile("collabsamples/hw_collaboration/contract.yaml")
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	collaborator1_tables, err := f.ReadFile("collabsamples/hw_collaboration/collaborator1_tables.yaml")
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	collaborator2_tables, err := f.ReadFile("collabsamples/hw_collaboration/collaborator2_tables.yaml")
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	// print(string(data))
 
-	outputDir := "collab_pkg"
+	outputDir := destPath + "/collab_files"
 	if _, err := os.Stat(outputDir); os.IsNotExist(err) {
 		err = os.MkdirAll(outputDir, 0755)
 		if err != nil {
 			log.Fatal(err)
+			return err
 		}
 	}
 
-	writeFilesToGivenOutputDir(outputDir, contractFile, collaborator1_tables, collaborator2_tables)
+	return writeFilesToGivenOutputDir(outputDir, contractFile, collaborator1_tables, collaborator2_tables)
 
 }
 
 // writing files to new directory, files data is fetched at runtime using embed package
-func writeFilesToGivenOutputDir(outputDir string, contractFile []byte, collaborator1_tables []byte, collaborator2_tables []byte) {
+func writeFilesToGivenOutputDir(outputDir string, contractFile []byte, collaborator1_tables []byte, collaborator2_tables []byte) error {
 	err := ioutil.WriteFile(filepath.Join(outputDir, "contract.yaml"), contractFile, 0644)
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 
 	err = ioutil.WriteFile(filepath.Join(outputDir, "collaborator1_tables.yaml"), collaborator1_tables, 0644)
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 
 	err = ioutil.WriteFile(filepath.Join(outputDir, "collaborator2_tables.yaml"), collaborator2_tables, 0644)
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
-	log.Println("File saved successfully!")
+	log.Printf("File saved successfully! %s", outputDir)
+	return nil
 }
